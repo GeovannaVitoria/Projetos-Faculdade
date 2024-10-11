e1ec820fd58044b01da50c450174c7c9dbb5b20e
Geovanna Vitória <137223302+GeovannaVitoria@users.noreply.github.com>
2024-10-10 21:10:35 -0300

    add aula06

---
+++ Python/aula06.py
@@ -0,0 +1,77 @@
+# a = [22, 8, 10]
+# b = [9, 21, 17]
+# c = a + b
+
+# print(f"C: {c}")
+
+# a = [1, 2, 3, 4, 5]
+# del a[2:4]
+
+# print(f"A: {a}")
+
+# lista = ["laranja", "azul", "verde", "preto", "rosa"]
+# lista.remove(" verde")
+# print(lista)
+
+# lista = ["laranja", "azul", "verde", "preto"]
+# lista.pop(0)
+# print(lista)
+
+# L = ["triangulo", "quadrado", "circulo", "retangulo"]
+# if "triangulo" in L:
+#     print("Sim, 'triangulo' esta presente na lista)
+
+
+# lista = ["laranja", "azul", "verde", "preto"]
+# lista.clear()
+# print(lista)
+
+# minha_lista = list(("carro", "casa", "moto"))
+# print(minha_lista)
+
+# L = list(range(100, 1100, 50)
+# print(L))
+
+# lista = ("Geo", 19, 1.65)
+# print(lista)
+
+# def usando_dicionario():
+#     medias = { "Geovanna": [7, 8, 10],
+#                 "Raphael": [10, 8, 7],
+#                 "Caio": [8, 5, 9]
+#             }
+
+#     nome = input("Informe o nome do aluno: ")
+#     if nome in medias:
+#         print(f"Media = {medias[nome]}")
+
+#     # medias["Marina"] = [10, 8, 6]
+#     # print(medias.keys())
+#     # print(medias.values())
+
+#     for maior in medias.values():
+#         soma = maior[0] + maior[1] + maior[2]
+#         medias = []
+#         soma /= 
+#         medias = soma.append()
+
+#     for nome, nota in medias.keys:
+#         if nota<7:
+#             print(f"{nome} reprovado!")
+
+# usando_dicionario()
+
+# artilharia_libertadores_24 = {"Júnior Santos": ['Botafogo', 10, 9],
+#                             "Miguel Borja": ['River Plate', 9, 6],
+#                             "Paulinho": ['Atlético Mineiro', 9, 6],
+#                             "Maximiliano Silvera": ['Peñarol', 10, 6],
+#                             "Pedro": ['Flamengo', 6, 5]}
+# print(artilharia_libertadores_24["Pedro"])
+
+carros = {"marca": "Ford",
+        "modelo": "Mustang",
+        "ano": 1966}
+
+carros["ano"] = int(input("Digite o ano do carro: "))
+
+print(carros)
